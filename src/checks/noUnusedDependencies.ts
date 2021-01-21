d3c2d78b8982a926e8da8fd03e30eb021e9c27d9
josehower <josehowerupn@gmail.com>
2021-01-21 17:39:05 +0100

    Add changes suggested on review

--- src/checks/noUnusedDependencies.ts
+++ src/checks/noUnusedDependencies.ts
@@ -6,28 +6,25 @@ export const title = 'No unused dependencies';
 
 export default async function noUnusedDependencies() {
   try {
-    const { stdout } = await execa.command('yarn depcheck');
+    const { stdout } = await execa.command(
+      'yarn depcheck --ignores="eslint,depcheck,eslint-*,babel-*,tslib,typescript,@typescript-eslint/*,@upleveled/*,@size-limit/*"',
+    );
 
     if (stdout.includes('No depcheck issue')) {
       return;
-    } else {
-      throw Error(stdout);
     }
   } catch (error) {
-    throw error.stdout
-      ? Error(
-          wordWrap(
-            `Remove next unused dependencies:
-             ${error.stdout
-               .split('\n')
-               .filter((str: string) => str.includes('* '))
-               .map((str: string) => str + '\n')
-               .join('')}
-               Remove these dependencies running the following command for each dependency:
-               ${commandExample('yarn remove <dependency name here>')}
-               `,
-          ),
-        )
-      : Error(error);
+    throw Error(
+      wordWrap(
+        `Remove next unused dependencies:
+           ${error.stdout
+             .split('\n')
+             .filter((str: string) => str.includes('* '))
+             .map((str: string) => str + '\n')
+             .join('')}
+             Remove these dependencies running the following command for each dependency:
+             ${commandExample('yarn remove <dependency name here>')}`,
+      ),
+    );
   }
 }
