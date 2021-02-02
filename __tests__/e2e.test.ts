52c9c79767fc204d2a010c3d9274595a3a53b853
Karl Horky <karl.horky@gmail.com>
2021-02-02 22:42:18 +0100

    Use git reset instead of copying and deleting

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -20,16 +20,10 @@ const testRepos: Repo[] = [
     repoPath: 'upleveled/preflight-test-project-react-passing',
     dirName: 'react-passing',
     installCommands: [
-      // Copy files from Git versions
-      'cp package.json package.committed.json',
-      'cp yarn.lock yarn.committed.lock',
       // To install the latest version of the ESLint config
       'yarn upgrade --latest @upleveled/eslint-config-upleveled',
-      // Move back originals to avoid Git commit
-      'rm package.json',
-      'rm yarn.lock',
-      'mv package.committed.json package.json',
-      'mv yarn.committed.lock yarn.lock',
+      // Avoid any issues with uncommitted files
+      'git reset --hard HEAD',
     ],
   },
 ];
