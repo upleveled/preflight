7eb3a62fd99cef91bffaa198df16f2622d33b8a0
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-11 18:13:46 +0200

    Update logic to fix error

--- src/checks/eslint.ts
+++ src/checks/eslint.ts
@@ -11,7 +11,7 @@ export default async function eslintCheck() {
     const lines = stdout.split('\n');
 
     // If no ESLint problems detected, throw the error
-    if (!lines.some((line) => line.match(/^\d+ problems?$/))) {
+    if (!/^\d+ problems?$/.test(lines[lines.length - 2]!)) {
       throw error;
     }
 
