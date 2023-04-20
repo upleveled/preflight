d0d08f4559cd2474cb7b223ab1dc1824c2a0d07b
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-20 20:52:56 +0200

    Update command to pnpm

--- src/checks/stylelint.ts
+++ src/checks/stylelint.ts
@@ -29,7 +29,7 @@ export default async function stylelintCheck() {
 
   try {
     await execaCommand(
-      `yarn stylelint **/*.{${supportedStylelintFileExtensions.join(
+      `pnpm stylelint **/*.{${supportedStylelintFileExtensions.join(
         ',',
       )}} --max-warnings 0 --formatter json`,
     );
