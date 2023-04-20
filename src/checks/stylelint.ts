1de52dfb3e47beab9affdf4243db5a1c45fed0b0
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-20 15:05:43 +0200

    Use join() instead of toString()

--- src/checks/stylelint.ts
+++ src/checks/stylelint.ts
@@ -8,7 +8,7 @@ export const title = 'Stylelint';
 export default async function stylelintCheck() {
   try {
     await execaCommand(
-      `yarn stylelint **/*.{${supportedExtensions.toString()}} --max-warnings 0 --formatter json`,
+      `yarn stylelint **/*.{${supportedExtensions.join()}} --max-warnings 0 --formatter json`,
     );
   } catch (error) {
     const { stdout } = error as { stdout: string };
