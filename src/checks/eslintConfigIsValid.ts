200a1b77ee610eb9d9a4cfe0eeed6797d942678d
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-20 20:40:06 +0200

    Update src/checks/eslintConfigIsValid.ts
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- src/checks/eslintConfigIsValid.ts
+++ src/checks/eslintConfigIsValid.ts
@@ -52,7 +52,7 @@ const config = {
 module.exports = config;`;
   } catch (err) {
     throw new Error(
-      `Error reading your .eslintrc.cjs file - please delete the file if exists and reinstall the config using the instructions on https://www.npmjs.com/package/eslint-config-upleveled
+      `Error reading your .eslintrc.cjs file - please delete the file if it exists and reinstall the config using the instructions on https://www.npmjs.com/package/eslint-config-upleveled
       `,
     );
   }
