2140b0de0112a2b721756c063a5be81c8e9c2c8e
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-21 09:48:32 +0200

    Update eslint check with Json output

--- src/checks/eslint.ts
+++ src/checks/eslint.ts
@@ -1,35 +1,37 @@
+import { ESLint } from 'eslint';
 import { execaCommand } from 'execa';
-import normalizeNewline from '../util/normalizeNewline';
+
+const errorKey = 'errorCount' as const;
 
 export const title = 'ESLint';
 
 export default async function eslintCheck() {
   try {
-    await execaCommand('pnpm eslint . --max-warnings 0  --format compact');
+    await execaCommand('pnpm eslint . --max-warnings 0  --format json');
   } catch (error) {
     const { stdout } = error as { stdout: string };
-    const lines = stdout.split('\n');
 
     // If no ESLint problems detected, throw the error
-    if (!/^\d+ problems?$/.test(lines[lines.length - 2]!)) {
+    if (!new RegExp(`"${errorKey}":`).test(stdout)) {
       throw error;
     }
 
     throw new Error(
       `ESLint problems found in the following files:
-        ${normalizeNewline(stdout)
-          .split('\n')
-          // Match lines starting with slashes (macOS, Linux) or drive letters (Windows)
-          .filter((line) => /^(\/|[A-Z]:\\)/.test(line))
-          // Strip out the filename
-          .map((line) => line.match(/^(([A-Z]:)?[^:]+):/)?.[1])
-          // Remove duplicate filenames
-          .reduce((linesWithoutDuplicates: string[], line) => {
-            if (line && !linesWithoutDuplicates.includes(line)) {
-              linesWithoutDuplicates.push(line);
-            }
-            return linesWithoutDuplicates;
-          }, [])
+        ${(JSON.parse(stdout) as ESLint.LintResult[])
+          .filter(
+            (stylelintResult) =>
+              !(
+                stylelintResult[errorKey] === 0 &&
+                stylelintResult.warningCount === 0
+              ),
+          )
+          .map(({ filePath }) =>
+            // Make paths relative to the project:
+            // before: /home/projects/next-student-project/app/api/hello/route.js
+            // after: app/api/hello/route.js
+            filePath.replace(process.cwd(), '').slice(1),
+          )
           .join('\n')}
 
         Open these files in your editor - there should be problems to fix
