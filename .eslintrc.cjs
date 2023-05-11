fb5fe1ab49abe39aa773753f4c2db4050e312665
Karl Horky <karl.horky@gmail.com>
2023-05-11 20:05:07 +0200

    Remove unncessary config, fix config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
   extends: ['upleveled'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json', './tsconfig.nonsrc.json'],
+  },
 };
 
 module.exports = config;
