ec7c24a02fba286935b861f8200f3b5225e33a66
Lukas Prochazka <prochazka.lu@gmail.com>
2024-02-02 17:43:22 +0100

    Add -p to mkdir

--- docker/clone-and-preflight.ts
+++ docker/clone-and-preflight.ts
@@ -71,7 +71,7 @@ if (projectUsesPostgresql) {
   process.env.PGPASSWORD = 'project_to_check';
 
   // Create directory for PostgreSQL socket
-  await executeCommand('mkdir /postgres-volume/run/postgresql');
+  await executeCommand('mkdir -p /postgres-volume/run/postgresql');
   await executeCommand('chown postgres:postgres /postgres-volume/run/postgresql');
 
   // Run script as postgres user to:
