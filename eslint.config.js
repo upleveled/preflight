83e3157be323969fbc2682b4f309afcb274a43c0
Karl Horky <karl.horky@gmail.com>
2024-05-05 20:09:10 +0200

    Avoid multiple parserOptions configuration objects

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,10 @@
 import upleveled from 'eslint-config-upleveled';
 
+upleveled[1].languageOptions.parserOptions = {
+  EXPERIMENTAL_useProjectService: true,
+};
+
 /** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray} */
-const config = [
-  ...upleveled,
-  {
-    languageOptions: {
-      parserOptions: {
-        EXPERIMENTAL_useProjectService: true,
-        project: './tsconfig.json',
-      },
-    },
-  },
-];
+const config = [...upleveled];
 
 export default config;
