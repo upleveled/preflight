f42606f5f18e56dc34df93524364ed12c6e4f4d7
Karl Horky <karl.horky@gmail.com>
2024-06-02 18:36:04 +0200

    Switch execaCommand back to execa

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -1,12 +1,12 @@
 import { tmpdir } from 'node:os';
 import { beforeAll, expect, test } from '@jest/globals';
-import { execa, execaCommand } from 'execa';
+import { execa } from 'execa';
 import pMap from 'p-map';
 
 const fixturesTempDir = process.env.GITHUB_ACTIONS
   ? // Switch to `tmpdir()` on GitHub Actions to avoid
-    // ESLint crashing with Windows paths over the 260
-    // character MAX_PATH limit
+    // ESLint crashing with Windows paths over the 260 character
+    // MAX_PATH limit
     // https://github.com/upleveled/preflight/pull/469/#issuecomment-1812422819
     // https://github.com/eslint/eslint/issues/17763
     // https://github.com/nodejs/node/issues/50753
@@ -20,30 +20,29 @@ beforeAll(
         {
           repoPath: 'upleveled/preflight-test-project-react-passing',
           dirName: 'react-passing',
-          installCommands: ['pnpm install --frozen-lockfile'],
+          async setup(this: { dirName: string }) {
+            await execa({
+              cwd: `${fixturesTempDir}/${this.dirName}`,
+            })`pnpm install --frozen-lockfile`;
+          },
         },
         {
           repoPath: 'upleveled/preflight-test-project-next-js-passing',
           dirName: 'next-js-passing',
-          installCommands: [
-            'pnpm install --frozen-lockfile',
+          async setup(this: { dirName: string }) {
+            await execa({
+              cwd: `${fixturesTempDir}/${this.dirName}`,
+            })`pnpm install --frozen-lockfile`;
             // Run project database migrations
-            'pnpm migrate up',
-          ],
+            await execa({
+              cwd: `${fixturesTempDir}/${this.dirName}`,
+            })`pnpm migrate up`;
+          },
         },
       ],
-      async ({ repoPath, dirName, installCommands }) => {
-        await execa`git clone --depth 1 --single-branch --branch=main https://github.com/${repoPath}.git ${fixturesTempDir}/${dirName} --config core.autocrlf=input`;
-        return pMap(
-          installCommands,
-          (command) =>
-            // TODO: Switch to parseCommandString when PR 1054 is published
-            // https://github.com/sindresorhus/execa/pull/1054#issuecomment-2143922502
-            execaCommand(command, {
-              cwd: `${fixturesTempDir}/${dirName}`,
-            }),
-          { concurrency: 1 },
-        );
+      async (testRepo) => {
+        await execa`git clone --depth 1 --single-branch --branch=main https://github.com/${testRepo.repoPath}.git ${fixturesTempDir}/${testRepo.dirName} --config core.autocrlf=input`;
+        return testRepo.setup();
       },
       {
         // Run up to 4 concurrent test repo setups at a time
