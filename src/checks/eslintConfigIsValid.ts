1ee10126d6fd11eede8198eb86ee9fe1b7ce0959
Karl Horky <karl.horky@gmail.com>
2024-08-29 19:51:58 +0200

    Format

--- src/checks/eslintConfigIsValid.ts
+++ src/checks/eslintConfigIsValid.ts
@@ -68,7 +68,8 @@ export default async function eslintConfigIsValid() {
   const eslintDisableOccurrences = [];
 
   for await (const { path } of readdirp('.', {
-    directoryFilter: (dir) => !['.git', '.next', 'node_modules'].includes(dir.basename),
+    directoryFilter: (dir) =>
+      !['.git', '.next', 'node_modules'].includes(dir.basename),
     fileFilter: (file) => /\.(?:js|jsx|ts|tsx)$/.test(file.basename),
   })) {
     const fileContents = await fs.readFile(path, 'utf-8');
