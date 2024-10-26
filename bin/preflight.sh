b94ce58d0d25848d4c9981395be99e5982604499
Karl Horky <karl.horky@gmail.com>
2024-10-26 17:25:51 +0200

    Try $script_dir path on Linux

--- bin/preflight.sh
+++ bin/preflight.sh
@@ -5,7 +5,7 @@ script_dir="$(cd "$(dirname "$0")" && pwd)"
 # Workaround for incorrect path in pnpm shim
 # https://github.com/pnpm/pnpm/issues/8704#issuecomment-2439618363
 #
-# TODO: Remove and switch back to "$script_dir/../node_modules/bin"
+# TODO: Remove and switch back to "$script_dir/../node_modules/.bin/tsx"
 # when the issue above is resolved
 tsx_shim="$script_dir/../node_modules/.bin/tsx"
 exec_command=$(awk '/^else$/{flag=1;next}/^fi$/{flag=0}flag' "$tsx_shim" | grep 'exec node')
@@ -13,4 +13,5 @@ cli_path=$(echo "$exec_command" | sed -E 's/^[[:space:]]*exec node[[:space:]]+"(
 cli_path="${cli_path/\$basedir/$script_dir}"
 cli_path="${cli_path/\/pnpm\//\/pnpm\/global\/5\/.pnpm\/}"
 
-node "$cli_path" "$script_dir/../src/index.ts"
+# node "$cli_path" "$script_dir/../src/index.ts"
+node "$script_dir/../node_modules/.bin/tsx" "$script_dir/../src/index.ts"
