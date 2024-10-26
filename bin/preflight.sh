236a1e57891849b15591fa7f57546532c7cd4a5a
Karl Horky <karl.horky@gmail.com>
2024-10-26 18:31:03 +0200

    Add back debugging

--- bin/preflight.sh
+++ bin/preflight.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
+echo "script_dir: $script_dir"
 
 # Workaround for incorrect path in pnpm shim
 # https://github.com/pnpm/pnpm/issues/8704#issuecomment-2439618363
@@ -12,6 +13,8 @@ exec_command=$(awk '/^else$/{flag=1;next}/^fi$/{flag=0}flag' "$tsx_shim" | grep 
 cli_path=$(echo "$exec_command" | sed -E 's/^[[:space:]]*exec node[[:space:]]+"([^"]+)".*/\1/')
 cli_path="${cli_path/\$basedir/$script_dir}"
 pnpm_bin_global="$(pnpm bin --global)"
+echo "pnpm_bin_global: $pnpm_bin_global"
 cli_path="${cli_path/$pnpm_bin_global\//$pnpm_bin_global\/global\/5\/.pnpm\/}"
+echo "cli_path: $cli_path"
 
 node "$cli_path" "$script_dir/../src/index.ts"
