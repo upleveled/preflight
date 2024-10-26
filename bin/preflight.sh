f391be9d1a4cda93c063470f888d1716374c1e0b
Karl Horky <karl.horky@gmail.com>
2024-10-26 19:00:00 +0200

    Remove commented code

--- bin/preflight.sh
+++ bin/preflight.sh
@@ -12,9 +12,5 @@ tsx_shim="$script_dir/../node_modules/.bin/tsx"
 exec_command=$(awk '/^else$/{flag=1;next}/^fi$/{flag=0}flag' "$tsx_shim" | grep 'exec node')
 cli_path=$(echo "$exec_command" | sed -E 's/^[[:space:]]*exec node[[:space:]]+"[^"]+(node_modules\/tsx\/[^"]+)".*/\1/')
 cli_path="$(pnpm bin --global)/global/5/.pnpm/$cli_path"
-# # pnpm_bin_global="$(pnpm bin --global)/global/5/.pnpm/$cli_path"
-# # echo "pnpm_bin_global: $pnpm_bin_global"
-# cli_path="${cli_path/$pnpm_bin_global\//$pnpm_bin_global\/global\/5\/.pnpm\/}"
-# echo "cli_path: $cli_path"
 
 node "$cli_path" "$script_dir/../src/index.ts"
