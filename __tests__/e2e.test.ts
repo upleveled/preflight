fa86381c0802d55398394b6bd3834e0c17cb9cff
Karl Horky <karl.horky@gmail.com>
2025-02-14 11:14:49 +0100

    Update timeout to 60s

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -75,7 +75,7 @@ test('Passes in the react-passing test project', async () => {
 
   expect(sortStdoutAndStripVersionNumber(stdout)).toMatchSnapshot();
   expect(stderr.replace(/^\(node:\d+\) /, '')).toMatchSnapshot();
-}, 45000);
+}, 60000);
 
 test('Passes in the next-js-passing test project', async () => {
   const { stdout, stderr } = await execa({
