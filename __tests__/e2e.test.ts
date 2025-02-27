5a954e3bae868fed88c248fffefb37cc3658a54d
Karl Horky <karl.horky@gmail.com>
2025-02-27 15:49:59 +0100

    Add logging

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -20,6 +20,11 @@ beforeAll(
     console.log(
       await execa`pnpm add --global --allow-build=esbuild ${process.cwd()}/${pnpmPackTarballPath}`,
     );
+    console.log(
+      (await execa`ls /c/Program\ Files/Git/home/runner/.local/share/pnpm`)
+        .stdout,
+    );
+    console.log((await execa`preflight --version`).stdout);
 
     await pMap(
       [
