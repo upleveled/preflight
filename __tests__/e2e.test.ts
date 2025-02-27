b3972887b828441f957e2f0ddbb539e724e5c7ae
Karl Horky <karl.horky@gmail.com>
2025-02-27 15:28:40 +0100

    Remove shell: 'bash'

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -15,9 +15,7 @@ beforeAll(
       throw new Error('Failed to find the tarball path in `pnpm pack` output');
     }
 
-    console.log(
-      await execa({ shell: 'bash', env: { SHELL: 'bash' } })`pnpm setup`,
-    );
+    console.log(await execa({ env: { SHELL: 'bash' } })`pnpm setup`);
     console.log(await execa({ shell: true })`echo $PATH`);
     console.log(await execa({ shell: 'bash' })`echo $PATH`);
     await execa`pnpm add --global --allow-build=esbuild ${process.cwd()}/${pnpmPackTarballPath}`;
