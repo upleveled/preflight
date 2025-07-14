298c7202b71be6d1fa3ca1916e9bc60542ee7868
Karl Horky <karl.horky@gmail.com>
2025-07-14 10:44:41 +0200

    Increase timeout from 60s to 70s for React test

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -80,7 +80,7 @@ test('Passes in the react-passing test project', async () => {
 
   expect(sortStdoutAndStripVersionNumber(stdout)).toMatchSnapshot();
   expect(stderr.replace(/^\(node:\d+\) /, '')).toMatchSnapshot();
-}, 60000);
+}, 70000);
 
 test('Passes in the next-js-passing test project', async () => {
   const { stdout, stderr } = await execa({
