e7f831db78972a33943bfa8e2ca4386ec9c5ea23
Karl Horky <karl.horky@gmail.com>
2025-08-07 14:47:12 +0200

    Format

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,11 @@
 import upleveled from 'eslint-config-upleveled';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  upleveled,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        project: './tsconfig.json',
-      },
+export default tseslint.config(upleveled, {
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      project: './tsconfig.json',
     },
   },
-);
+});
