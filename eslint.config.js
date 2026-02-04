e692abd015e24805211c162b857f8b6f125f2758
Karl Horky <karl.horky@gmail.com>
2026-02-04 11:15:14 +0100

    Simplify ESLint config to avoid `projectService` error

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,1 @@
-import upleveled from 'eslint-config-upleveled';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(upleveled, {
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      project: './tsconfig.json',
-    },
-  },
-});
+export { default } from 'eslint-config-upleveled';
