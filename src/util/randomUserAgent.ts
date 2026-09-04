c596addb11604487c804c8b1ab3448963cb6b5a6
Karl Horky <karl.horky@gmail.com>
2026-09-04 19:52:09 +0200

    Switch from Replit to StackBlitz (#769)

--- src/util/randomUserAgent.ts
+++
@@ -1,6 +0,0 @@
-import userAgents from 'top-user-agents';
-
-export function randomUserAgent() {
-  const randomIndex = Math.floor(Math.random() * (userAgents.length - 1));
-  return userAgents[randomIndex]!;
-}
